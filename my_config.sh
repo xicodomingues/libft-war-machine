836d39cb659c816ecb4e7258d403e95eae78ec4b
Francisco Domingues <fsoares-@student.42barcelona.com>
2022-01-24 17:12:31 +0100

    change to hava private sources folder

--- my_config.sh
+++ my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT="../srcs/"
+PATH_LIBFT="../__my_srcs/"
 
 #If you use a relative path, don't put double quotes.
 #For example :
